c16092bb6f27be8edd4cef0581fc0620f3700a39
Lucas Tymen <lucas.tymen@gmail.com>
2022-11-17 15:03:01 +0100

    -controller et view

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,11 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  validates_uniqueness_of :movie, :scope => [:list_id]
+  validates :movie, presence: true
+  validates :list, presence: true
+  has_many :Bookmark
+  has_many :lists
+  validates :comment, length: { minimum: 6 }
+
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,6 @@
 class List < ApplicationRecord
+  validates :name, presence: true
+  validates_uniqueness_of :name, :scope => [:name]
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks, dependent: :destroy
 end
